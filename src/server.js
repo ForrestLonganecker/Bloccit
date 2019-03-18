131e5c2866d6ba9c266cd4c76937f0b639e3179c
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-18 08:49:18 -0700

    WIP deploy to production take 2

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 server.listen(port);
 
 function normalizePort(val) {
-    const port = parseInt(val, 20);
+    const port = parseInt(val, 10);
     if(isNaN(port)) {
         return val;
     }
