d912cc905354a119beae8aecb098e60b8e308f9a
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-25 17:58:29 -0700

    fixed typo that was causing npm test glitch

--- spec/integration/advertisments_spec.js
+++ spec/integration/advertisments_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const sequelize = require("../../src/db.models/index").sequelize;
+const sequelize = require("../../src/db/models/index").sequelize;
 const Advertisement = require("../../src/db/models").Advertisement;
 const base = "http://localhost:3000/advertisements/";
 
@@ -24,18 +24,25 @@ describe("routes: advertisements", () => {
         });
     });
 
-    describe("GET /advertisments", () => {
-
+    describe("GET /advertisements", () => {
         it("should return a status code 200 and all advertisements", (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(err).toBeNull();
-                expect(body).toContain("Buy stuff!");
-                expect(body).toContain("New gear!");
+                expect(body).toContain("Advertisements");
+                expect(body).toContain("Buy Stuff!");
                 done();
             });
         });
-
     });
 
+    // describe("GET /advertisements/new", () => {
+    //     it("should render a new advertisement form", (done) => {
+    //         request.get(`${base}new`, (err, res, body) => {
+    //             expect(err).toBeNull();
+    //             expect(body).toContain("New Advertisement");
+    //             done();
+    //         });
+    //     });
+    // });
 });
