62715a90a7ab974d24fb287ca7dbda7a471e8534
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-26 12:09:43 -0700

    topic controller resource pt2 assignment completed

--- spec/integration/advertisments_spec.js
+++ spec/integration/advertisments_spec.js
@@ -36,13 +36,101 @@ describe("routes: advertisements", () => {
         });
     });
 
-    // describe("GET /advertisements/new", () => {
-    //     it("should render a new advertisement form", (done) => {
-    //         request.get(`${base}new`, (err, res, body) => {
-    //             expect(err).toBeNull();
-    //             expect(body).toContain("New Advertisement");
-    //             done();
-    //         });
-    //     });
-    // });
+    describe("GET /advertisements/new", () => {
+        it("should render a new advertisement form", (done) => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New Advertisement");
+                done();
+            });
+        });
+    });
+
+    describe("GET /advertsements/:id", () => {
+        it("should render a view with the selected advertisement", (done) => {
+            request.get(`${base}${this.advertisement.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Buy Stuff!");
+                done();
+            });
+        });
+    });
+
+    describe("GET /advertisements/:id/edit", () => {
+        it("should render a view with an edit advertisement form", (done) => {
+            request.get(`${base}${this.advertisement.id}/edit`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Edit Advertisement");
+                expect(body).toContain("Buy Stuff!");
+                done();
+            });
+        });
+    });
+
+    describe("POST /advertisements/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "Big bags",
+                description: "So many colors"
+            }
+        };
+
+        it("should create a new advertisement and redirect", (done) => {
+            request.post(options, (err, res, body) => {
+                Advertisement.findOne({where: {title: "Big bags"}})
+                .then((advertisement) => {
+                    expect(res.statusCode).toBe(303);
+                    expect(advertisement.title).toBe("Big bags");
+                    expect(advertisement.description).toBe("So many colors");
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+        
+    });
+
+    describe("POST /advertisements/:id/destroy", () => {
+        it("should delete the advertisement with the associated ID", (done) => {
+            Advertisement.all()
+            .then((advertisements) => {
+                const advertisementCountBeforeDelete = advertisements.length;
+                expect(advertisementCountBeforeDelete).toBe(1);
+                request.post(`${base}${this.advertisement.id}/destroy`, (err, res, body) => {
+                    Advertisement.all()
+                    .then((advertisements) => {
+                        expect(err).toBeNull();
+                        expect(advertisements.length).toBe(advertisementCountBeforeDelete - 1);
+                        done();
+                    })
+                });
+            });
+        });
+    });
+
+    describe("POST /advertisements/:id/update", () => {
+        it("should update the topic with the given values", (done) => {
+            const options = {
+                url: `${base}${this.advertisement.id}/update`,
+                form: {
+                    title: "Buy lots of stuff!",
+                    description: "New/Used gear!"
+                }
+            };
+            request.post(options, (err, res, body) => {
+                expect(err).toBeNull();
+                Advertisement.findOne({
+                    where: { id: this.advertisement.id }
+                })
+                .then((advertisement) => {
+                    expect(advertisement.title).toBe("Buy lots of stuff!");
+                    done();
+                });
+            });
+        });
+    });
 });
--- src/routes/advertisements.js
+++ src/routes/advertisements.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const advertismentController = require("../controllers/advertisementController");
+const advertisementController = require("../controllers/advertisementController");
 
-router.get("/advertisements", advertismentController.index);
+router.get("/advertisements", advertisementController.index);
+router.get("/advertisements/new", advertisementController.new);
+router.get("/advertisements/:id", advertisementController.show);
+router.get("/advertisements/:id/edit", advertisementController.edit)
+router.post("/advertisements/create", advertisementController.create);
+router.post("/advertisements/:id/destroy", advertisementController.destroy);
+router.post("/advertisements/:id/update", advertisementController.update);
 
 module.exports = router;
