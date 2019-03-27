5a303c824a338e89fe5c676cf2011ad03404b891
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-27 09:09:15 -0700

    post resource pt1 checkpoint completed

--- src/db/migrations/20190327152616-create-post.js
+++ src/db/migrations/20190327152616-create-post.js
@@ -1,17 +1,19 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Banners', {
+    return queryInterface.createTable('Posts', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      source: {
+      title: {
+        allowNull: false,
         type: Sequelize.STRING
       },
-      description: {
+      body: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
@@ -25,6 +27,7 @@ module.exports = {
       topicId: {
         type: Sequelize.INTEGER,
         onDelete: "CASCADE",
+        allowNull: false,
         references: {
           model: "Topics",
           key: "id",
@@ -34,6 +37,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Banners');
+    return queryInterface.dropTable('Posts');
   }
 };
